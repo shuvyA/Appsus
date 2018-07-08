a0f2f2c1f547dc5641df28242a5a02353cdc9e0e
Shuvy <shuvyan@gmail.com>
2018-07-09 02:07:19 +0300

    no link index html

--- js/pages/home-cmp.js
+++ js/pages/home-cmp.js
@@ -4,8 +4,7 @@ export default {
         <div class="cover-img  text-cover flex align-center column justify-center space-around">
         <h1 class="animated swing">App-Sus</h1>
 <div>
-    <button class="btn-cover" type="button" onclick="openGallery()">Keep</button>
-    <button class="btn-cover" type="button" onclick="openGallery()">E-mail</button>
+   
 </div>
         
     </div>
